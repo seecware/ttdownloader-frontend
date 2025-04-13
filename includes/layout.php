a05b610de476d36713008e6cc15c49ccbff033b2
Francisco Mendoza <seecware@gmail.com>
2025-04-13 13:32:33 -0600

    ADDING NEW STRUCTURE TO LAYOUT FILE. New structure, better. html head body header content footer

--- includes/layout.php
+++ includes/layout.php
@@ -1,2 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/public/css/styles.css" rel="stylesheet">
+    <title>Home</title>
+</head>
+
+<body>
 <?php
-include $parent_content;
+include __DIR__ . "/header.php"; ?>
+<main class="min-h-screen w-full max-w-6xl mx-auto pt-10 px-4"><?php include $parent_content; ?></main>
+<?php include __DIR__ . "/footer.php";?>
+</body>
+</html>
