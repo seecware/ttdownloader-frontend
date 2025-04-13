2e691149a9ae0db0efac176ac92636d1149ebb09
Francisco Mendoza <seecware@gmail.com>
2025-04-13 13:26:33 -0600

    Adding header html fragment.

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<header class="bg-white shadow">
+    <div class="h-16 max-w-7xl mx-auto p-4 flex justify-between items-center">
+        <div class="flex h-full items-end">
+            <h1 class="text-xl ml-2 font-bold">TikTok Downloader</h1>
+        </div>
+      <nav>
+        <a href="/index.php" class="text-blue-600 hover:underline mr-4">Inicio</a>
+        <a href="/pages/contacto.php" class="text-blue-600 hover:underline">Contacto</a>
+      </nav>
+    </div>
+</header>
